225b6cdc5e9b1ad41cfdf057c4f942ba1ff55b25
Angelica Farias Oliveira <98922466+AngelicaOliveira22@users.noreply.github.com>
2022-05-20 20:09:12 -0300

    Add files via upload

---
+++ Aula 2 - Respondendo ao usuário.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Programa que leia o nome de uma pessoa e mostre uma mensagem  de Boas-Vindas:
+nome = input("Digite seu nome: ")
+print("É um prazer te conhecer, {}!".format(nome))
+
