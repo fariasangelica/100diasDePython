d807218324573f5fabab902a73c1312cc8b1fde4
Angelica Farias Oliveira <98922466+fariasangelica@users.noreply.github.com>
2022-05-23 17:54:43 -0300

    Add files via upload

---
+++ Aula 5 - Antecessor e Sucessor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+n = int(input("Digite um número: "))
+print("Analisando o valor {}, seu antecessor é {} e o sucessor {}.".format(n,(n-1),(n+1)))
+
