f01de0bf7beb3a436b3b8dfa243f3c38bb87157a
Angelica Farias Oliveira <98922466+fariasangelica@users.noreply.github.com>
2022-05-24 13:53:50 -0300

    Add files via upload

---
+++ Aula  6 - Dobro, Triplo, Raiz Quadrada.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#Crie um algoritmo qye leia um número e mosre o seu dobro, triplo e raiz quadrada:
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n * (1/2)
+print('o dobro de {} vale {}.'.format(n, d))
+print('O tiplo de {} vale {}.\nA raiz de {} é igual a {:.2f}.'.format(n, t, n, r))
+
+
+# In[ ]:
+
+
+
+
