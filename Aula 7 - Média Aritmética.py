54849a117217f546b612d1c951710b19c4cba281
Angelica Farias Oliveira <98922466+fariasangelica@users.noreply.github.com>
2022-05-28 21:13:59 -0300

    Add files via upload

---
+++ Aula 7 - Média Aritmética.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+n1 = float(input("Primeira nota do aluno: "))
+n2 = float(input("Segunda nota do aluno: "))
+média = (n1 + n2) / 2
+print("A média entre {:.1f} e {:.1f} é igual a {:.1f}.".format(n1, n2, média))
+
